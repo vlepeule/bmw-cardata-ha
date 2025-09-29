ed9d3cd244dcc263ac60023d83e4b769ab1119ad
Jyri Saukkonen <jyri.saukkonen+jjyksi@gmail.com>
2025-09-29 20:59:44 +0300

    API testing

--- custom_components/cardata/entity.py
+++ custom_components/cardata/entity.py
@@ -100,7 +100,7 @@ class CardataEntity(RestoreEntity):
             return
         self._attr_name = new_name
         if write_state and self.hass:
-            self.async_write_ha_state()
+            self.schedule_update_ha_state()
 
     async def async_added_to_hass(self) -> None:
         await super().async_added_to_hass()
