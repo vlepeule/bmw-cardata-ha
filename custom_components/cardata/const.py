af44c072172b95c5d76ae08c24a73db617607a13
Jyri Saukkonen <jyri.saukkonen+jjyksi@gmail.com>
2025-09-30 19:28:04 +0300

    API testing

--- custom_components/cardata/const.py
+++ custom_components/cardata/const.py
@@ -51,7 +51,11 @@ HV_BATTERY_DESCRIPTORS = [
     "vehicle.drivetrain.electricEngine.charging.level",
     "vehicle.powertrain.electric.battery.stateOfHealth.displayed",
     "vehicle.vehicleIdentification.basicVehicleData",
-    "vehicle.drivetrain.batteryManagement.batterySizeMax"
+    "vehicle.drivetrain.batteryManagement.batterySizeMax",
+    "vehicle.drivetrain.batteryManagement.maxEnergy",
+    "vehicle.powertrain.electric.battery.charging.power",
+    "vehicle.drivetrain.electricEngine.charging.status"
+
 ]
 
 #fetch_vehicle_mapping returns data like this:
